c8c8111ebee3ce20d49595d28287e87c1bd4eeae
YvonneGi <giryvonne@gmail.com>
2019-11-01 15:07:43 +0200

    create authentication form

---
+++ hood/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    # url(r'^search/', views.search_results, name='search_results'),
+    # url(r'^explore/', views.explore, name = 'explore'),
+    # url(r'^accounts/profile/(\d+)', views.profile, name = 'profile'),
+    # url(r'^new/post/', views.new_post, name = 'new-post'),
+    # url(r'^accounts/edit-profile/', views.edit_profile, name = 'edit-profile'),
+]
+
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+# from django.core.exceptions import ObjectDoesNotExist
+# from .models import Post,Profile,Comment,Like,Follow,User
+# from .forms import NewPostForm,ProfileForm,CommentForm,LikeForm,FollowForm
+# from django.db.models import Q
+# import datetime as dt
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    
+    return render(request,'welcome.html')
