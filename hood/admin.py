42f63d93aa4661ba79a2d18eb1975077edabb6a0
YvonneGi <giryvonne@gmail.com>
2019-11-02 18:45:10 +0200

    update admin.py file

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(Profile)
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Join)
+
